24f6d13d277989b457836d175166de4266bf68e6
Kat Kuntz <rezonantcheetah@gmail.com>
2020-11-03 15:53:25 -0500

    Add YGO Card representation

---
+++ YGOProgressionHelper/YGOCard.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YGOProgressionHelper
+{
+    // CardList myDeserializedClass = JsonConvert.DeserializeObject<CardList>(myJsonResponse); 
+    public class CardSet
+    {
+        public string set_name { get; set; }
+        public string set_code { get; set; }
+        public string set_rarity { get; set; }
+        public string set_rarity_code { get; set; }
+        public string set_price { get; set; }
+    }
+
+    public class CardImage
+    {
+        public int id { get; set; }
+        public string image_url { get; set; }
+        public string image_url_small { get; set; }
+    }
+
+    public class CardPrice
+    {
+        public string cardmarket_price { get; set; }
+        public string tcgplayer_price { get; set; }
+        public string ebay_price { get; set; }
+        public string amazon_price { get; set; }
+        public string coolstuffinc_price { get; set; }
+    }
+
+    public class YGOCard
+    {
+        public int id { get; set; }
+        public int count { get; set; }
+        public string name { get; set; }
+        public string type { get; set; }
+        public string desc { get; set; }
+        public int atk { get; set; }
+        public int def { get; set; }
+        public int level { get; set; }
+        public string race { get; set; }
+        public string attribute { get; set; }
+        public int scale { get; set; }
+        public int linkval { get; set; }
+        public List<CardSet> card_sets { get; set; }
+        public List<CardImage> card_images { get; set; }
+        public List<CardPrice> card_prices { get; set; }
+    }
+
+    public class CardList
+    {
+        public List<YGOCard> data { get; set; }
+    }
+
+
+}
